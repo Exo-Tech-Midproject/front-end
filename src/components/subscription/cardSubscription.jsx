818b1c742eac0caf5f8cc0af50a756b897b8aba1
AnasNemrawi <anasnemrawi3000@gmail.com>
2023-10-13 19:24:19 +0000

    fixing the subscription page

--- src/components/subscription/cardSubscription.jsx
+++ src/components/subscription/cardSubscription.jsx
@@ -1,75 +1,96 @@
-import React from 'react'
-// import Rating from '@mui/material/Rating';
-import "./cardSubscription.css";
+import React from 'react';
+import './cardSubscription.css';
+import doc from "./doc.png"
 
-
-
-
-export default function cardSubscription() {
-
+export default function CardSubscription() {
 	const cardsData = [
 		{
 			id: 1,
 			name: 'Hasan',
 			bio: 'caring specialist with 3 years of experience. They prioritize patient well-being, advocate for preventive care, and contribute to medical research.',
-			rating: 3.5
+			rating: 3,
+			department: "Doctor",
+			img: doc,
+			profile: "http://localhost:3000/dashboard/subscriptions",
 		},
 		{
 			id: 2,
 			name: 'Anas',
 			bio: 'caring specialist with 3 years of experience. They prioritize patient well-being, advocate for preventive care, and contribute to medical research.',
-			rating: 3.5
+			rating: 4,
+			department: "Doctor",
+			img: doc,
+			profile: "http://localhost:3000/dashboard/subscriptions",
 		},
 		{
 			id: 3,
 			name: 'Tasneem',
 			bio: 'caring specialist with 3 years of experience. They prioritize patient well-being, advocate for preventive care, and contribute to medical research.',
-			rating: 3.5
+			rating: 2,
+			department: "Doctor",
+			img: doc,
+			profile: "http://localhost:3000/dashboard/subscriptions",
 		},
 		{
 			id: 4,
 			name: 'Abdullah',
 			bio: 'caring specialist with 3 years of experience. They prioritize patient well-being, advocate for preventive care, and contribute to medical research.',
-			rating: 3.5
+			department: "Doctor",
+			img: doc,
+			profile: "http://localhost:3000/dashboard/subscriptions",
+			rating: 5,
 		},
+		{
+			id: 4,
+			name: 'Abdullah',
+			bio: 'caring specialist with 3 years of experience. They prioritize patient well-being, advocate for preventive care, and contribute to medical research.',
+			department: "Doctor",
+			img: doc,
+			profile: "http://localhost:3000/dashboard/subscriptions",
+			rating: 5,
+		},
+		{
+			id: 4,
+			name: 'Abdullah',
+			bio: 'caring specialist with 3 years of experience. They prioritize patient well-being, advocate for preventive care, and contribute to medical research.',
+			department: "Doctor",
+			img: doc,
+			profile: "http://localhost:3000/dashboard/subscriptions",
+			rating: 5,
+		},
+
+
 	];
 
+	const renderStars = (rating) => {
+		const stars = [];
+		for (let i = 1; i <= 5; i++) {
+			const starClass = i <= rating ? 'star filled' : 'star';
+			stars.push(<span key={i} className={starClass}>&#9733;</span>);
+		}
+		return stars;
+	};
+
 	return (
-		<div class='mainP'>
-
-
+		<div className="mainP">
 			{cardsData.map((card) => (
-				<div class="card">
+				// <a key={card.id} href={card.profile}>
+					<div className="Subcard">
+						<div className="Subcard-text">
+							<div className="Subportada" style={{ backgroundImage: `url(${card.img})` }}></div>
 
-					<div class="card-text">
-						<div class="portada">
-
-						</div>
-						<div class="title-total">
-							<div class="title">Doctor</div>
-							<h3>{card.name}</h3>
-
-							<div class="desc">	{card.bio}</div>
-
-							<div class="rating">
-								{/* <button><i class="far fa-heart"></i></button>
-								<button><i class="far fa-envelope"></i></button>
-								<button><i class="fas fa-user-friends"></i></button> */}
-								{/* <Rating
-									sx={{
-										height: "2px",
-										width: "5px",
-										fontSize: "15px"
-									}} name="half-rating-read" defaultValue={card.rating} precision={0.5} readOnly /> */}
-
+							<div className="Subtitle-total">
+								<div className="Subtitle">{card.department}</div>
+								<h3>{card.name}</h3>
+								<div className="Subdesc">{card.bio}</div>
+								<div className="rating">{renderStars(card.rating)}</div>
 							</div>
 						</div>
-
 					</div>
-				</div>
+				// </a>
 			))}
-
 		</div>
 
-	)
+
+	);
 }

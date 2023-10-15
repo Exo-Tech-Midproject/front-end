533b99993d8880acbaa96b6ffe0431d06fab8079
Haznto <haznto@gmail.com>
2023-10-15 19:42:24 +0000

    profile-page-edited

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,36 +1,67 @@
 // import { Box, Grid, } from '@mui/material'
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import React from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import ProfileHeadSection from './ProfileHeadSection';
 import SidePageSection from './SidePageSection';
 import InfoSection from './InfoSection';
 import PrescriptionsSection from './PrescriptionsSection';
+import { LoginContext } from "../../ContextApi/Auth";
+import axios from "axios";
+import cookie from 'react-cookies'
+import jwtDecode from 'jwt-decode';
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import ProfileCalendar from "./ProfileCalendar";
+let DBRUL = process.env.REACT_APP_BASE_URL
 
 export default function Profile() {
+    const [profileInfo, setProfileInfo] = useState(null)
+    let token = cookie.load('auth')
+    const payload = jwtDecode(token)
+    const containerRef = useRef(null);
+    const getProfileInfo = async () => {
+        try {
+            const response = await axios.get(`http://localhost:5000/${payload.accountType}/${payload.username}/profile`, {
+                headers: { Authorization: `Bearer ${token}` }
+            });
+
+            let data = response.data
+            setProfileInfo(data)
+            return data
+        } catch (error) {
+            console.error('Error uploading image:', error);
+        }
+
+    }
+    useEffect(() => {
+        getProfileInfo()
+    }, [])
     return (
         <Grid container bgcolor={'#f2f5f9'} rowGap={4} >
 
 
             <Grid item xs={12} md={8} paddingLeft={4} paddingTop={4} >
-                <ProfileHeadSection />
+                <ProfileHeadSection getProfileInfo={getProfileInfo} profileInfo={profileInfo} setProfileInfo={setProfileInfo} />
             </Grid >
 
             <Grid item xs={12} md={4} pl={4} pt={4}>
-                <SidePageSection />
+                <Box ref={containerRef} p={2} borderRadius='15px' boxShadow='rgba(159, 162, 191, 0.18) 0px 9px 16px, rgba(159, 162, 191, 0.32) 0px 2px 2px' sx={{ height: '100%', overflow: 'auto', backgroundColor: 'white' }}>
+                    <ProfileCalendar containerRef={containerRef} />
+                </Box>
             </Grid>
 
             <Grid item xs={12} sm={12} md={8} paddingLeft={4} paddingTop={4} >
-                <Box display='flex' flexDirection='column' flexWrap='wrap' gap={2}>
-                    <InfoSection />
-                    {/* <InfoSection /> */}
-                </Box>
+
+                <InfoSection getProfileInfo={getProfileInfo} profileInfo={profileInfo} setProfileInfo={setProfileInfo} />
+                {/* <InfoSection /> */}
+
             </Grid >
 
             <Grid item xs={12} md={4} pl={4} pt={4}  >
                 {/* <SidePageSection /> */}
                 {/* <SidePageSection /> */}
-                <PrescriptionsSection />
+                <PrescriptionsSection getProfileInfo={getProfileInfo} profileInfo={profileInfo} setProfileInfo={setProfileInfo} />
             </Grid>
 
 
--- src/components/Profile/ProfileHeadSection.jsx
+++ src/components/Profile/ProfileHeadSection.jsx
@@ -9,11 +9,17 @@ import React, { useEffect, useState } from 'react'
 import axios from "axios";
 import cookie from 'react-cookies'
 import jwtDecode from 'jwt-decode';
+import UploadPfpCardModal from "./UploadPfpModal";
+import UploadPfpModal from "./UploadPfpModal";
+import UploadCoverModal from "./UploadCover";
 let DBRUL = process.env.REACT_APP_BASE_URL
 
-export default function ProfileHeadSection() {
+export default function ProfileHeadSection({ getProfileInfo, profileInfo, setProfileInfo }) {
     const [selectedFile, setSelectedFile] = useState(null)
-    const [profileInfo, setProfileInfo] = useState(null)
+    const [selectedCover, setSelectedCover] = useState(null)
+    // const [profileInfo, setProfileInfo] = useState(null)
+    const [showModal, setShowModal] = useState(false)
+    const [showModal2, setShowModal2] = useState(false)
     let token = cookie.load('auth')
     const payload = jwtDecode(token)
     const handleImageChange = (event) => {
@@ -21,11 +27,29 @@ export default function ProfileHeadSection() {
 
 
     };
+    const handleCoverChange = (event) => {
+        setSelectedCover(event.target.files[0]);
+
+
+    };
+
+    function handleShowModal() {
+        setShowModal(true)
+    }
+    function handleCloseModal() {
+        setShowModal(false)
+    }
+    function handleShowModal2() {
+        setShowModal2(true)
+    }
+    function handleCloseModal2() {
+        setShowModal2(false)
+    }
     const handleUpload = async () => {
         const formData = new FormData();
         formData.append('image', selectedFile);
         try {
-            const response = await axios.post(`http://localhost:5000/patient/${payload.username}/uploadpfp`, formData, {
+            const response = await axios.post(`http://localhost:5000/${payload.accountType}/${payload.username}/uploadpfp`, formData, {
                 headers: {
                     Authorization: `Bearer ${token}`,
                     'Content-Type': 'multipart/form-data'
@@ -34,28 +58,35 @@ export default function ProfileHeadSection() {
             });
             setProfileInfo(getProfileInfo())
             console.log('Image uploaded successfully:', response.data);
+            handleCloseModal()
+        } catch (error) {
+            console.error('Error uploading image:', error);
+        }
+    };
+    const handleUploadCover = async () => {
+        const formData = new FormData();
+        formData.append('image', selectedCover);
+        try {
+            const response = await axios.post(`http://localhost:5000/${payload.accountType}/${payload.username}/uploadcover`, formData, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'multipart/form-data'
+                },
+
+            });
+            setProfileInfo(getProfileInfo())
+            console.log('Image uploaded successfully:', response.data);
+            handleCloseModal2()
         } catch (error) {
             console.error('Error uploading image:', error);
         }
     };
 
-    const getProfileInfo = async () => {
-        try {
-            const response = await axios.get(`http://localhost:5000/patient/${payload.username}/profile`, {
-                headers: { Authorization: `Bearer ${token}` }
-            });
 
-            let data = response.data
-            setProfileInfo(data)
-            return data
-        } catch (error) {
-            console.error('Error uploading image:', error);
-        }
-    }
 
-    useEffect(() => {
-        getProfileInfo()
-    }, [])
+    // useEffect(() => {
+    //     getProfileInfo()
+    // }, [profileInfo])
     return (
         <>
             <Box borderRadius={'15px'} marginBottom={'30px'}  >
@@ -65,18 +96,20 @@ export default function ProfileHeadSection() {
 
 
             <Box maxHeight='70%' position='relative' display='flex' boxShadow='rgba(159, 162, 191, 0.18) 0px 9px 16px, rgba(159, 162, 191, 0.32) 0px 2px 2px' >
-                <Button variant='contained' sx={{ position: 'absolute', bottom: '0', right: '0', bgcolor: '#4B6477', ":hover": { bgcolor: '#1F485B' } }} startIcon={<FileUploadOutlinedIcon />}> Change Cover</Button>
+                <Button onClick={handleShowModal2} variant='contained' sx={{ position: 'absolute', bottom: '-34px', right: '0', bgcolor: '#4B6477', ":hover": { bgcolor: '#1F485B' } }} startIcon={<FileUploadOutlinedIcon />}> Change Cover</Button>
                 <Avatar
-                    sx={{ width: '130px', height: '130px', position: 'absolute', left: '20px', bottom: '-55px' }}
+                    sx={{ width: '130px', height: '130px', position: 'absolute', left: '20px', bottom: '-80px', border: 'solid 2px #1F485BAA' }}
                     src={profileInfo?.profileImg || 'https://cdn5.vectorstock.com/i/1000x1000/27/89/user-account-flat-icon-vector-14992789.jpg'}
 
                 >
                 </Avatar>
-                <input onChange={handleImageChange} type='file' />
-                <IconButton onClick={handleUpload} size='small' sx={{ position: 'absolute', left: '120px', bottom: '-55px', bgcolor: '#4B6477 ', color: 'white', opacity: "0.9", zIndex: '300', ":hover": { bgcolor: '#1F485Bf9' } }}><FileUploadOutlinedIcon /></IconButton>
-                <img style={{ borderRadius: '15px', objectFit: 'cover', width: '100%', maxHeight: '350px' }} alt='cover-img' src='https://www.slideegg.com/image/catalog/700940-PowerPoint%20Background%20Templates%20Medical.png' />
-                <Typography variant='h5' position={'absolute'} left='165px' bottom='-45px' color='#1F485B'>Hasan Tommalieh</Typography >
+
+                <IconButton onClick={handleShowModal} size='small' sx={{ position: 'absolute', left: '120px', bottom: '-80px', bgcolor: '#4B6477 ', color: 'white', opacity: "0.9", zIndex: '300', ":hover": { bgcolor: '#1F485Bf9' } }}><FileUploadOutlinedIcon /></IconButton>
+                <img style={{ borderRadius: '15px', objectFit: 'cover', width: '100%', height: '300px' }} alt='cover-img' src={profileInfo?.coverImg || 'https://www.slideegg.com/image/catalog/700940-PowerPoint%20Background%20Templates%20Medical.png'} />
+                <Typography variant='h5' position={'absolute'} left='165px' bottom='-65px' color='#1F485B'>{profileInfo?.fullName}</Typography >
             </Box>
+            <UploadPfpModal handleImageChange={handleImageChange} showModal={showModal} handleCloseModal={handleCloseModal} handleUpload={handleUpload} />
+            <UploadCoverModal handleCoverChange={handleCoverChange} showModal2={showModal2} handleCloseModal2={handleCloseModal2} handleUploadCover={handleUploadCover} />
         </>
     )
 }

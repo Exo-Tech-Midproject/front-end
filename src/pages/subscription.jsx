73805f527cceb15f07894c301377a9a7a202afce
AnasNemrawi <anasnemrawi3000@gmail.com>
2023-10-16 18:46:02 +0000

    linking subscrption

--- src/pages/subscription.jsx
+++ src/pages/subscription.jsx
@@ -1,13 +1,11 @@
 
 import CardSubscription from '../components/subscription/cardSubscription';
-import CardSubscriptionLink from '../components/subscription/cardSubscriptionLink';
 import HeroSubscription from '../components/subscription/heroSubscription';
 export default function subscription() {
 	return (
 		<>
 			<HeroSubscription />
-			{/* <CardSubscription /> */}
-			<CardSubscriptionLink />
+			<CardSubscription />
 		</>
 	)
 }

88a97792b776b6eed8a8913cafa4d78f76a4c877
AnasNemrawi <anasnemrawi3000@gmail.com>
2023-10-17 09:34:41 +0000

    adding rating

--- src/pages/subscription.jsx
+++ src/pages/subscription.jsx
@@ -2,14 +2,12 @@
 import CardSubscription from '../components/subscription/cardSubscription';
 import HeroSubscription from '../components/subscription/heroSubscription';
 
-import SubscriptionsLink from '../components/subscription/SubscriptionsLink';
 
 export default function subscription() {
 	return (
 		<>
 			<HeroSubscription />
-			{/* <CardSubscription /> */}
-			<SubscriptionsLink />
+			<CardSubscription />
 		</>
 	)
 }

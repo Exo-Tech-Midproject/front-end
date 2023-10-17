70b441c2035a1b4d132850000e26af0de2678112
AnasNemrawi <anasnemrawi3000@gmail.com>
2023-10-17 08:53:56 +0000

    added rating

--- src/components/subscription/cardSubscription.jsx
+++ src/components/subscription/cardSubscription.jsx
@@ -10,30 +10,48 @@ import jwtDecode from "jwt-decode";
 import cookie from 'react-cookies'
 let DBURL = process.env.REACT_APP_BASE_URL
 
+
 export default function CardSubscription() {
-	const [subs, setSubs] = useState(null)
+	const [subs, setSubs] = useState(null);
 
-	async function fetchUserSubscrptions() {
-		try {
-			let token = cookie.load("auth")
-			let payload = await jwtDecode(token)
-
-			let userSub = await axios.get(`${DBURL}/physician/${payload.username}/patients/subscribers`,
-				{
-					headers: { Authorization: `Bearer ${token}` }
-				})
-			console.log("data=> ", userSub.data)
-			setSubs(userSub.data)
-			console.log()
-
-		} catch (err) {
-			console.log(err)
-		}
-	}
 	useEffect(() => {
-		fetchUserSubscrptions()
+		// Define the function as an async function within the useEffect to properly use state
+		async function fetchUserSubscriptions() {
+			try {
+				let token = cookie.load("auth");
+				let payload = await jwtDecode(token);
 
-	}, [])
+				if (payload?.accountType === 'patient') {
+					let userSub = await axios.get(
+						`${DBURL}/patient/${payload.username}/physicians/subscriptions`,
+						{
+							headers: { Authorization: `Bearer ${token}` }
+						}
+					);
+					console.log("data for patients => ", userSub.data);
+					console.log("data=> ", userSub.data)
+					setSubs(userSub.data)
+					console.log()
+				}
+
+				if (payload?.accountType === 'physician') {
+					let userSub = await axios.get(
+						`${DBURL}/physician/${payload.username}/patients/subscribers`,
+						{
+							headers: { Authorization: `Bearer ${token}` }
+						}
+					);
+					console.log("data for physicians => ", userSub.data);
+					setSubs(userSub.data);
+				}
+			} catch (err) {
+				console.log(err);
+			}
+		}
+
+		fetchUserSubscriptions();
+	}, []);
+
 	let img = doc
 	return (
 		<div className="mainP">
@@ -42,18 +60,18 @@ export default function CardSubscription() {
 					<div className="Subcard" key={index}>
 						<div className="Subcard-text">
 
-							<div className="Subportada" style={{ backgroundImage: `url(${img})` }} >{/*`url(${user.profileImg})`*/}</div>
+							<div className="Subportada" style={{ backgroundImage: `url(${user.profileImg})` }} ></div>
 							<div className="Subtitle-total">
-								{/* <AuthPatient> */}
-								<div className="Subtitle">"{user.department}"</div>
-								{/* </AuthPatient> */}
+								<AuthPhysician>
+									<h3>{user.insurance}</h3>
+								</AuthPhysician>
 								<h3>{user.fullName}</h3>
 								<h5>{user.emailAddress}</h5>
-								<div className="Subtitle">{user.mobileNumber}</div>
-								<h3>{user.insurance}</h3>
-								{/* <AuthPhysician> */}
-								<Rating sx={{ marginLeft: "25px" }} name="half-rating-read" defaultValue={user.rating} precision={0.5} size="small" readOnly />
-								{/* </AuthPhysician> */}
+								<h4>{user.mobileNumber}</h4>
+								<AuthPatient>
+									<div className="Subtitle">{user.department}</div>
+									<Rating sx={{ marginLeft: "25px" }} name="half-rating-read" defaultValue={user.rating} precision={0.5} size="medium" readOnly />
+								</AuthPatient>
 							</div>
 						</div>
 					</div>

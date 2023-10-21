6409290f3b44089722f57e98e0c29e0c07938040
AnasNemrawi <anasnemrawi3000@gmail.com>
2023-10-21 20:04:00 +0000

    updating

--- src/components/Cards/TeamCard.jsx
+++ src/components/Cards/TeamCard.jsx
@@ -5,92 +5,102 @@ import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 import TwitterIcon from "@mui/icons-material/Twitter";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import img from "./image.png"
 
 const CustomLink = ({ children, }) => (
-  <Link
-  sx={{
-    width: "1rem", 
-    cursor: "pointer",
-    transition: "transform 0.2s, fill 0.2s, stroke-width 0.2s",
-    color: " #003870", 
-    strokeWidth: "2px",
-    
-    "&:hover":{
-     transform: "scale(1.25)",
-     color: "#007EFC",
-     strokeWidth: "2px",
-     transitionDuration: "0.2s"
-    }
- }}
-  >
-    {children}
-  </Link>
+	<Link
+		sx={{
+			width: "1rem",
+			cursor: "pointer",
+			transition: "transform 0.2s, fill 0.2s, stroke-width 0.2s",
+			color: " #003870",
+			strokeWidth: "2px",
+
+			"&:hover": {
+				transform: "scale(1.25)",
+				color: "#007EFC",
+				strokeWidth: "2px",
+				transitionDuration: "0.2s"
+			}
+		}}
+	>
+		{children}
+	</Link>
 );
 
-const ServiceCard = ({ title, subtitle }) => {
-  return (
-    <Box sx={{
-  padding: "0.75rem",
-  width: "18rem",
-  height: "7rem",
-  backgroundColor: "#fff",
-  borderRadius: "0.375rem",
-  boxShadow: "0px 0px 0.25rem rgba(0, 0, 0, 0.2)",
-    }}>
-      <Stack direction="row" spacing={2} width="100%" alignItems="center" justifyContent="center">
-        <Box sx={{
-          width: "3.5rem",
-          height: "3.5rem",
-          borderRadius: "50%",
-          boxShadow: "0px 0px 0.25rem rgba(0, 0, 0, 0.2)",
-          background: "linear-gradient(to right, #F9C97C, #A2E9C1)",
-          cursor: "pointer",
-          transition: "transform 0.3s",
-          "&:hover":{
-            transform: "scale(1.1)",
-            background: "linear-gradient(to right, #C9A9E9, #7EE7FC)",
-          }
-        }}>
-          <Stack direction="row"  width="100%" height="100%" alignItems="center" justifyContent="center">
-          <AccountCircleOutlinedIcon sx={{
-              width: "1.75rem",
-              color: "#003870",
-          }}/>
-          </Stack>
-         
-        </Box>
-        <Box sx={{
-          borderLeft: "1px solid #ccc",
-          margin: "0.75rem",}}>
-          <Stack direction="column" spacing={1} width="100%" height="100%" alignItems="center" justifyContent="center">
-          <Typography sx={{
-            paddingLeft: "0.75rem",
-            color: "#888",
-            fontWeight: "600",
-            fontSize: "0.875rem",
-          }}>{title}</Typography>
-          <Typography sx={{
-            paddingLeft: "0.75rem",
-            background: "linear-gradient(to left, #005BC4, #27272A)",
-            WebkitBackgroundClip: "text",
-            backgroundClip: "text",
-            color: "transparent",
-            fontWeight: "100px",
-            fontSize: "1.20rem",
-          }}>{subtitle}</Typography>
-          <Box>
-            <Stack direction="row" spacing={3} width="100%" alignItems="center" justifyContent="center">
-              <CustomLink ><FacebookRoundedIcon fontSize= "15px" /></CustomLink>
-              <CustomLink><TwitterIcon fontSize= "15px"/></CustomLink>
-              <CustomLink><GitHubIcon fontSize= "15px"/></CustomLink>
-            </Stack>
-          </Box>
-          </Stack>
-        </Box>
-      </Stack>
-    </Box>
+const TeamCard = ({ title, subtitle }) => {
+	return (
+		<Box sx={{
+			padding: "1rem",
+			width: "27rem",
+			height: "10rem",
+			backgroundColor: "#fff",
+			borderRadius: "0.375rem",
+			boxShadow: "0px 0px 0.25rem rgba(0, 0, 0, 0.2)",
+			alignItems:"center",
+			justifyContent:"space-around",
+		}}>
+			<Stack direction="row" spacing={0} width="100%" alignItems="center" justifyContent="space-around">
+				<Box sx={{
+					width: "8rem",
+					height: "8rem",
+					borderRadius: "50%",
+					boxShadow: "0px 0px 0.25rem rgba(0, 0, 0, 0.2)",
+					background: "linear-gradient(to right, #F9C97C, #A2E9C1)",
+					cursor: "pointer",
+					transition: "transform 0.3s",
+					"&:hover": {
+						transform: "scale(1.1)",
+						background: "linear-gradient(to right, #C9A9E9, #7EE7FC)",
+					}
+				}}>
+					{/* <Stack direction="row" width="100%" height="100%" alignItems="center" justifyContent="center"> */}
+					{/* <AccountCircleOutlinedIcon sx={{
+							width: "1.75rem",
+							color: "#003870",
+						}} /> */}
+					<img src={img} alt="team img" style={{
+						width: "8rem",
+						height: "8rem",
+						borderRadius: "50%",
+						color: "#003870",
+					}} />
+					{/* </Stack> */}
 
-  );
+				</Box>
+				<Box sx={{
+					borderLeft: "1px solid #ccc",
+					margin: "0.75rem",
+				}}>
+					<Stack direction="column" spacing={1} width="100%" height="100%" alignItems="center" justifyContent="center">
+						<Typography sx={{
+							paddingLeft: "0.75rem",
+							color: "#888",
+							fontWeight: "600",
+							fontSize: "0.875rem",
+						}}>{title}</Typography>
+						<Typography sx={{
+							paddingLeft: "0.75rem",
+							background: "linear-gradient(to left, #005BC4, #27272A)",
+							WebkitBackgroundClip: "text",
+							backgroundClip: "text",
+							color: "transparent",
+							fontWeight: "100px",
+							fontSize: "1.20rem",
+						}}>{subtitle}</Typography>
+						<Box>
+							<Stack direction="row" spacing={3} width="100%" alignItems="center" justifyContent="center">
+								<CustomLink ><FacebookRoundedIcon fontSize="15px" /></CustomLink>
+								<CustomLink><TwitterIcon fontSize="15px" /></CustomLink>
+								<CustomLink><GitHubIcon fontSize="15px" /></CustomLink>
+							</Stack>
+						</Box>
+					</Stack>
+				</Box>
+			</Stack>
+		</Box>
+
+	);
 };
 
-export default ServiceCard;
+export default TeamCard;
--- src/containers/QASection.jsx
+++ src/containers/QASection.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 import Title from '../components/Title';
 import Navbar from '../components/Navbars/MainNavbar'
-import { Stack, Box, Button, } from "@mui/material"
+import { Stack, Box, } from "@mui/material"
 import Typography from '@mui/material/Typography';
 import Footer from '../components/Footers/MainFooter'
 import Breadcrumbs from '@mui/material/Breadcrumbs';
@@ -9,240 +10,136 @@ import HomeIcon from '@mui/icons-material/Home';
 import PanoramaFishEyeIcon from '@mui/icons-material/PanoramaFishEye';
 import Link from '@mui/material/Link';
 import ContactSupportOutlinedIcon from '@mui/icons-material/ContactSupportOutlined';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import Q from "../components/Q&A/Q"
+import Post from "../components/Q&A/post"
+import AuthPatient from '../components/Auths/AuthPatient';
+// import AuthPhysician from '../components/Auths/AuthPhysician';
+import axios from 'axios';
+import jwtDecode from "jwt-decode";
+import cookie from 'react-cookies'
+let DBURL = process.env.REACT_APP_BASE_URL
+
 function QASection() {
-  return (
-    <Stack direction="column" >
-      <Navbar />
-      <Box sx={{
-        mt: 8,
-        height: "250px",
-        background: "#112731",
-        backgroundImage: 'url("https://images.pexels.com/photos/3652097/pexels-photo-3652097.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
-        backgroundSize: '100%',
-        backgroundRepeat: 'no-repeat',
-        objectFit: 'cover',
-        backgroundPosition: 'center',
-        backgroundBlendMode: 'overlay'
-      }}
-      >
-        <Stack sx={{ height: "100%" }} alignItems="center" justifyContent="center">
-          <Title
-            variant={{ xs: "h3", sm: "h2", md: "h1" }}
-            sx={{ letterSpacing: "0.02em", mb: 1, color: "white" }}
-          >
-            Questions
-          </Title>
-
-          <Breadcrumbs aria-label="breadcrumb">
-            <Link
-              href="/"
-              underline="hover"
-              sx={{
-                display: 'flex',
-                alignItems: 'center'
-              }}
-              color="white"
-
-            >
-              <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-              Home
-            </Link>
-            <Link
-              href="/QACategories"
-              underline="hover"
-              sx={{
-                display: 'flex',
-                alignItems: 'center'
-              }}
-              color="white"
-
-            >
-              <ContactSupportOutlinedIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-              Category
-            </Link>
-            <Typography
-              sx={{ display: 'flex', alignItems: 'center', textDecoration: "underline" }}
-              color="#67ABCB"
-            >
-              <PanoramaFishEyeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-              Questions
-            </Typography>
-          </Breadcrumbs>
-
-        </Stack>
-      </Box>
-      <Box sx={{ width: "100%", bgcolor: "white", height: "100vh" }}>
-      <Stack direction="row" spacing={4} alignItems="center" justifyContent="center" sx={{ height: "100%" }} >
-      <Box sx={{
-          width: "80%",
-          height: "fit-content",
-          backgroundColor: "white",
-          boxShadow: "0px 187px 75px rgba(0, 0, 0, 0.01), 0px 105px 63px rgba(0, 0, 0, 0.05), 0px 47px 47px rgba(0, 0, 0, 0.09), 0px 12px 26px rgba(0, 0, 0, 0.1), 0px 0px 0px rgba(0, 0, 0, 0.1)",
-          borderRadius: "17px 17px 27px 27px",
-        }}>
-        <Box p={3}>
-          <Box>
-            <Stack spacing={1} direction="row" alignItems="center"  sx={{width: "100%", }}>
-              <AccountCircleOutlinedIcon sx={{ color: "black" }} />
-              <Stack direction="column" spacing={0.4}>
-                <Typography sx={{
-                  fontWeight: "700",
-                  fontSize: "12px",
-                  color: "#47484b",
-                }}>Tasneem Hassasneh</Typography>
-              </Stack>
-            </Stack>
-          </Box>
-
-        </Box>
-        <Box sx={{
-          width: "100%",
-          height: "fit-content",
-          backgroundColor: "#f1f1f1",
-          padding: "8px",
-        }}>
-          <Box sx={{
-            backgroundColor: "#ffffff",
-            borderRadius: "8px 8px 21px 21px",
-            padding: "8px",
-          }}>
-             <Box ><textarea style={{
-              width: "100%",
-              height: "40px",
-              resize: "none",
-              border: "0",
-              borderRadius: "6px",
-              padding: "12px 12px 10px 12px",
-              fontSize: "13px",
-              outline: "none",
-              caretColor: "#0a84ff",
-             }} placeholder='Ask Question'></textarea></Box>
-          <Box>
-            <Button sx={{
-              width: "30px",
-              height: "30px",
-              backgroundColor: "#0a84ff",
-              margin: "0 0 0 auto",
-              "&:hover": {
-                backgroundColor: "#026eda",
-              }
-            }}  variant="contained">
-              SEND
-            </Button>     
-          </Box>
-          </Box>
-         
-        </Box>
-    </Box>
-        <Box sx={{
-          width: "450px",
-          height: "fit-content",
-          backgroundColor: "white",
-          boxShadow: "0px 187px 75px rgba(0, 0, 0, 0.01), 0px 105px 63px rgba(0, 0, 0, 0.05), 0px 47px 47px rgba(0, 0, 0, 0.09), 0px 12px 26px rgba(0, 0, 0, 0.1), 0px 0px 0px rgba(0, 0, 0, 0.1)",
-          borderRadius: "17px 17px 27px 27px",
-        }}>
-          <Box>
-            <Typography sx={{
-              width: "100%",
-              height: "50px",
-              position: "relative",
-              display: "flex",
-              alignItems: "center",
-              paddingLeft: "20px",
-              borderBottom: "1px solid #f1f1f1",
-              fontWeight: 700,
-              fontSize: "13px",
-              color: "#47484b",
-              "&:after": {
-                content: "''",
-                width: "8ch",
-                height: "1px",
-                position: "absolute",
-                bottom: "-1px",
-                backgroundColor: "#47484b",
-            }
-          }}>Qusetion</Typography>
-        </Box>
-        <Box p={3}>
-          <Box>
-            <Stack spacing={1} direction="row" alignItems="center"  sx={{width: "100%", }}>
-              <AccountCircleOutlinedIcon sx={{ color: "black" }} />
-              <Stack direction="column" spacing={0.4}>
-                <Typography sx={{
-                  fontWeight: "700",
-                  fontSize: "12px",
-                  color: "#47484b",
-                }}>Tasneem Hassasneh</Typography>
-                <Typography sx={{
-                  fontWeight: "600",
-                  fontSize: "10px",
-                  color: "#acaeb4",
-                }}>Wednesday, Oct 18th at 2:45pm</Typography>
-              </Stack>
-            </Stack>
-          </Box>
-          <Box p={1}>
-            <Typography sx={{
-              fontSize: "12px",
-              lineHeight: "16px",
-              fontWeight: 600,
-              color: "#5f6064",
-            }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography>
-          </Box>
-
-        </Box>
-        <Box sx={{
-          width: "100%",
-          height: "fit-content",
-          backgroundColor: "#f1f1f1",
-          padding: "8px",
-        }}>
-          <Box sx={{
-            backgroundColor: "#ffffff",
-            borderRadius: "8px 8px 21px 21px",
-            padding: "8px",
-          }}>
-             <Box ><textarea style={{
-              width: "100%",
-              height: "40px",
-              resize: "none",
-              border: "0",
-              borderRadius: "6px",
-              padding: "12px 12px 10px 12px",
-              fontSize: "13px",
-              outline: "none",
-              caretColor: "#0a84ff",
-             }} placeholder='Reply'></textarea></Box>
-          <Box>
-    
-            
-            <Button sx={{
-              width: "30px",
-              height: "30px",
-              backgroundColor: "#0a84ff",
-              margin: "0 0 0 auto",
-              "&:hover": {
-                backgroundColor: "#026eda",
-              }
-            }}  variant="contained">
-              <ArrowUpwardIcon />
-            </Button>
-            
-          </Box>
-          </Box>
-         
-        </Box>
-    </Box>
+	const [post, setpost] = useState([]);
 
 
-      </Stack >
-     
-    </Box >
-      <Footer />
-    </Stack >
-  );
+	async function fetchUserpost() {
+		try {
+			let token = cookie.load("auth");
+			let payload = await jwtDecode(token);
+			console.log("payload", payload)
+
+			if (payload?.accountType === 'patient') {
+				let userPost = await axios.get(
+					`${DBURL}/patient/${payload.username}/Q&A`,
+					{
+						headers: { Authorization: `Bearer ${token}` }
+					}
+				);
+
+				setpost(userPost.data);
+				console.log("userPost.data", userPost.data);
+				return userPost.data
+			}
+			if (payload?.accountType === 'physician') {
+				let userPost = await axios.get(
+					`${DBURL}/physician/${payload.username}/Q&A`,
+					{
+						headers: { Authorization: `Bearer ${token}` }
+					}
+				);
+
+				setpost(userPost.data);
+				console.log("userPost.data", userPost.data);
+				return userPost.data
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	useEffect(() => {
+		fetchUserpost();
+	}, []);
+
+
+	return (
+		<Stack direction="column" >
+			<Navbar />
+			<Box sx={{
+				mt: 8,
+				height: "250px",
+				background: "#112731",
+				backgroundImage: 'url("https://images.pexels.com/photos/3652097/pexels-photo-3652097.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
+				backgroundSize: '100%',
+				backgroundRepeat: 'no-repeat',
+				objectFit: 'cover',
+				backgroundPosition: 'center',
+				backgroundBlendMode: 'overlay'
+			}}
+			>
+				<Stack sx={{ height: "100%" }} alignItems="center" justifyContent="center">
+					<Title
+						variant={{ xs: "h3", sm: "h2", md: "h1" }}
+						sx={{ letterSpacing: "0.02em", mb: 1, color: "white" }}
+					>
+						Questions
+					</Title>
+
+					<Breadcrumbs aria-label="breadcrumb">
+						<Link
+							href="/"
+							underline="hover"
+							sx={{
+								display: 'flex',
+								alignItems: 'center'
+							}}
+							color="white"
+
+						>
+							<HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+							Home
+						</Link>
+						<Link
+							href="/QACategories"
+							underline="hover"
+							sx={{
+								display: 'flex',
+								alignItems: 'center'
+							}}
+							color="white"
+
+						>
+							<ContactSupportOutlinedIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+							Category
+						</Link>
+						<Typography
+							sx={{ display: 'flex', alignItems: 'center', textDecoration: "underline" }}
+							color="#67ABCB"
+						>
+							<PanoramaFishEyeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+							Questions
+						</Typography>
+					</Breadcrumbs>
+
+				</Stack>
+			</Box>
+			<Box sx={{ width: "100%", bgcolor: "#F2F8FF", height: "100%", position: "relative" }} py={11}>
+				<Stack direction="column" spacing={4} alignItems="center" justifyContent="center" sx={{ height: "100%" }} >
+					<AuthPatient>
+						<Q fetchUserpost={fetchUserpost} setpost={setpost} />
+					</AuthPatient>
+
+					{post?.map(singlePost => (
+						<Post data={singlePost} key={singlePost.id} fetchUserpost={fetchUserpost} setpost={setpost} />
+
+					))}
+
+				</Stack >
+
+			</Box >
+			<Footer />
+		</Stack >
+	);
 }
-
 export default QASection
